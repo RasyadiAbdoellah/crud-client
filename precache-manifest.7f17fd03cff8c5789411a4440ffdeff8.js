f6c83b0845117d010ecb9bffa652982c7009fc98
RasyadiAbdoellah <rfabdoellah@gmail.com>
2019-06-22 01:05:38 +0700

    Updates

---
+++ precache-manifest.7f17fd03cff8c5789411a4440ffdeff8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c820a07c1d1c1ec00591dd84b6d3ff97",
+    "url": "/crud-client/index.html"
+  },
+  {
+    "revision": "027c3c3b514c49eeee46",
+    "url": "/crud-client/static/css/main.103b76a2.chunk.css"
+  },
+  {
+    "revision": "a110786159a8d251b3b0",
+    "url": "/crud-client/static/js/2.bb129d04.chunk.js"
+  },
+  {
+    "revision": "027c3c3b514c49eeee46",
+    "url": "/crud-client/static/js/main.1b198d69.chunk.js"
+  },
+  {
+    "revision": "9cf35034349039979bcc",
+    "url": "/crud-client/static/js/runtime~main.ee355b39.js"
+  }
+]);
